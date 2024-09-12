74ae20ce5083239966b14d9be655e2c71db2d9cf
Kenedy Duwarah <kenedyduwarah24@gmail.com>
2024-09-12 08:28:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-//this script is for hello world 
+let display = document.getElementById("display");
 
 function appendToDisplay(value) {
-    document.getElementById('display').value += value;
+    display.value += value;
 }
 
 function clearDisplay() {
-    document.getElementById('display').value = '';
+    display.value = "";
 }
 
 function deleteLast() {
-    let display = document.getElementById('display');
     display.value = display.value.slice(0, -1);
 }
 
 function calculate() {
-    document.getElementById('display').value = 'Hello World';
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
 }
